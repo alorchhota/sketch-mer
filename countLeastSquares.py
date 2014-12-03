c333b2b0f2e0cc035182d6402b4e24c8d7696312
Princy Parsana <princy@Princys-MacBook-Pro.local>
2014-12-03 10:27:06 -0500

    commenting heap function temporarily for speeding up the process

--- countLeastSquares.py
+++ countLeastSquares.py
@@ -31,7 +31,7 @@ class countLeastSquares(object):
         Cormode and Muthukrishnan prove that for an item i with count a_i, the
         estimate from the sketch a_i_hat will satisfy the relation
 
-        a_hat_i <= a_i + epsilon * ||a||_1
+        a_hat_i <= a_i + epsilon * ||x||_1
 
         with probability at least 1 - delta, where a is the the vector of all
         all counts and ||x||_1 is the L1 norm of a vector x
@@ -60,16 +60,16 @@ class countLeastSquares(object):
             raise ValueError("delta must be between 0 and 1, exclusive")
         if epsilon <= 0 or epsilon >= 1:
             raise ValueError("epsilon must be between 0 and 1, exclusive")
-        if k < 1:
-            raise ValueError("k must be a positive integer")
+        # if k < 1:
+        #     raise ValueError("k must be a positive integer")
 
         self.w = int(np.ceil(np.exp(1) / epsilon))
         self.d = int(np.ceil(np.log(1 / delta)))
-        self.k = k
+        # self.k = k
         self.hash_functions = [self.__generate_hash_function() for i in range(self.d)]
         self.count = np.zeros((self.d, self.w), dtype='int32')
         self.n = 0
-        self.heap, self.top_k = [], {} # top_k => [estimate, key] pairs
+        # self.heap, self.top_k = [], {} # top_k => [estimate, key] pairs
 
     def update(self, key, increment):
         """
@@ -93,41 +93,41 @@ class countLeastSquares(object):
             column = hash_function(abs(hash(key)))
             self.count[row, column] += increment
 
-        self.update_heap(key)
+        # self.update_heap(key)
 
-    def update_heap(self, key):
-        """
-        Updates the class's heap that keeps track of the top k items for a
-        given key
-
-        For the given key, it checks whether the key is present in the heap,
-        updating accordingly if so, and adding it to the heap if it is
-        absent
-
-        Parameters
-        ----------
-        key : string
-            The item to check against the heap
-
-        """
-        estimate = self.get(key)
-
-        if not self.heap or estimate >= self.heap[0][0]:
-            if key in self.top_k:
-                old_pair = self.top_k.get(key)
-                old_pair[0] = estimate
-                heapq.heapify(self.heap)
-            else:
-                if len(self.top_k) < self.k:
-                    heapq.heappush(self.heap, [estimate, key])
-                    self.top_k[key] = [estimate, key]
-                else:
-                    new_pair = [estimate, key]
-                    old_pair = heapq.heappushpop(self.heap, new_pair)
-                    old_pair = old_pair[1]
-                    if key != old_pair:
-                        self.top_k.pop(old_pair)
-                        self.top_k[key] = new_pair
+    # def update_heap(self, key):
+    #     """
+    #     Updates the class's heap that keeps track of the top k items for a
+    #     given key
+    #
+    #     For the given key, it checks whether the key is present in the heap,
+    #     updating accordingly if so, and adding it to the heap if it is
+    #     absent
+    #
+    #     Parameters
+    #     ----------
+    #     key : string
+    #         The item to check against the heap
+    #
+    #     """
+    #     estimate = self.get(key)
+    #
+    #     if not self.heap or estimate >= self.heap[0][0]:
+    #         if key in self.top_k:
+    #             old_pair = self.top_k.get(key)
+    #             old_pair[0] = estimate
+    #             heapq.heapify(self.heap)
+    #         else:
+    #             if len(self.top_k) < self.k:
+    #                 heapq.heappush(self.heap, [estimate, key])
+    #                 self.top_k[key] = [estimate, key]
+    #             else:
+    #                 new_pair = [estimate, key]
+    #                 old_pair = heapq.heappushpop(self.heap, new_pair)
+    #                 old_pair = old_pair[1]
+    #                 if key != old_pair:
+    #                     self.top_k.pop(old_pair)
+    #                     self.top_k[key] = new_pair
 
     def get(self, key):
         """
